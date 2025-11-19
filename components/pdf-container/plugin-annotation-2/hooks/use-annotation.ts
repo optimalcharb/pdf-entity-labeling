40bf9ed04fe9942ddc506fa3ebed4ea45850a514
optimalcharb <zcharliebauer@gmail.com>
2025-11-19 17:35:47 -0500

    refactor: remove useAnnotation

--- components/pdf-container/plugin-annotation-2/hooks/use-annotation.ts
+++ components/pdf-container/plugin-annotation-2/hooks/use-annotation.ts
@@ -1,23 +1,5 @@
 import { useCapability, usePlugin } from "@embedpdf/core/react"
-import { useEffect, useState } from "react"
-import { AnnotationPlugin, AnnotationState } from "../lib"
-import { initialState } from "../lib/state"
+import { AnnotationPlugin } from "../lib"
 
 export const useAnnotationPlugin = () => usePlugin<AnnotationPlugin>(AnnotationPlugin.id)
 export const useAnnotationCapability = () => useCapability<AnnotationPlugin>(AnnotationPlugin.id)
-
-export const useAnnotation = () => {
-  const { provides } = useAnnotationCapability()
-  const [state, setState] = useState<AnnotationState>(initialState)
-
-  useEffect(() => {
-    return provides?.onStateChange((action) => {
-      setState(action)
-    })
-  }, [provides])
-
-  return {
-    state,
-    provides,
-  }
-}
