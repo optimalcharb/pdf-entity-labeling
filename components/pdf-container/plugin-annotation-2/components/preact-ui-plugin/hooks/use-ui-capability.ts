a4f4c3bf4b03702d8e2ab5a477c9ac9a41d530fa
optimalcharb <zcharliebauer@gmail.com>
2025-11-23 00:00:29 -0500

    revert: delete preact-plugin-ui

--- components/pdf-container/plugin-annotation-2/components/preact-ui-plugin/hooks/use-ui-capability.ts
+++
@@ -1,4 +0,0 @@
-import { useCapability } from "@embedpdf/core/preact"
-import { UIPlugin } from "../ui-plugin"
-
-export const useUICapability = () => useCapability<UIPlugin>(UIPlugin.id)
--- components/pdf-container/plugin-annotation-2/components/preact-ui-plugin/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./components"
-export * from "./hooks/use-ui-capability"
-export * from "./lib"
